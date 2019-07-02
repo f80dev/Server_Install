82900e770bd077d2dc1a75950bca8290d39f9177
F80 <43803343+f80dev@users.noreply.github.com>
2019-07-02 17:58:09 +0200

    Update setup.sh

--- odoo/setup.sh
+++ odoo/setup.sh
@@ -7,5 +7,10 @@ sh entrypoint.sh
 wget https://raw.githubusercontent.com/odoo/docker/master/12.0/odoo.conf
 mkdir /var/lib/odoo
 
-docker run -d -e POSTGRES_USER=odoo -e POSTGRES_PASSWORD=odoo -e POSTGRES_DB=postgres --name db postgres:10
-docker run -v /root/odoo:/etc/odoo -p 8069:8069 --name odoo --link db:db -t odoo
+#docker rm -f db
+#docker rm -f odoo
+
+docker run -d --restart=always -e POSTGRES_USER=odoo -e POSTGRES_PASSWORD=odoo -e POSTGRES_DB=postgres --name db postgres:10
+docker run -v /root/odoo:/etc/odoo -p 8069:8069 --name odoo --link db:db -d --restart=always -t odoo
+
+echo "lancer http://192.168.1.5:8069"
