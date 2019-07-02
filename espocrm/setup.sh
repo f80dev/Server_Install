0af9f5b7467069e7fa8b6422337b9e8cd2e47e61
F80 <43803343+f80dev@users.noreply.github.com>
2019-07-02 17:08:47 +0200

    Update setup.sh

--- espocrm/setup.sh
+++ espocrm/setup.sh
@@ -1,7 +1,13 @@
 cd /root
 git clone https://github.com/peters5/docker-espocrm.git
 
-cd docker-espocrm
+mkdir /var/www
+mkdir /var/www/espocrm
+mkdir /var/www/espocrm/data
+cd /var/www/espocrm
+find data -type d -exec chmod 775 {} + && chown -R 100:101 .
+
+cd /root/docker-espocrm
 docker build -t "espocrm" .
 docker-compose up
 
