0ac5094db21fecce292b4f2499d54263347723f9
F80 <43803343+f80dev@users.noreply.github.com>
2019-07-15 00:01:26 +0200

    Update dolibarr.sh

--- dolibarr.sh
+++ dolibarr.sh
@@ -11,3 +11,22 @@ wget https://sourceforge.net/projects/dolibarr/files/Dolibarr%20installer%20for%
 apt-get install mime-support
 dpkg -i dolibarr_10.0.0-4_all.deb
 
+
+#Installation manuel
+cd /root
+wget https://sourceforge.net/projects/dolibarr/files/Dolibarr%20ERP-CRM/10.0.0/dolibarr-10.0.0.zip
+unzip dolibarr-10.0.0.zip
+sudo mkdir /var/www/html/dolibarr
+sudo cp -r dolibarr-10.0.0/htdocs/* /var/www/html/dolibarr/
+sudo chown -R www-data:www-data /var/www/html/dolibarr/
+sudo chmod -R 755 /var/www/html/dolibarr/
+
+mkdir /var/documents
+chown www-data:www-data /var/documents
+chmod 700 /var/documents
+
+sudo nano /etc/apache2/sites-available/dolibarr.conf
+
+sudo a2ensite dolibarr
+sudo a2enmod rewrite
+sudo systemctl restart apache2
