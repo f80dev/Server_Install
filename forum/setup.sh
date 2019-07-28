b6bb1129c4ff5fc5b173bd9108d66fe0ab4ac61b
F80 <43803343+f80dev@users.noreply.github.com>
2019-07-28 22:56:28 +0200

    Create setup.sh

---
+++ forum/setup.sh
@@ -0,0 +1,41 @@
+echo "Create SMF Database"
+
+#sudo mysql -u root -p
+#CREATE DATABASE smf;
+#CREATE USER 'smfuser'@'localhost' IDENTIFIED BY 'hh';
+#GRANT ALL ON smf.* TO 'smfuser'@'localhost' IDENTIFIED BY 'hh' WITH GRANT OPTION;
+#FLUSH PRIVILEGES;
+#EXIT;
+
+cd /tmp && wget https://download.simplemachines.org/index.php/smf_2-0-15_install.zip
+sudo mkdir -p /var/www/html/smf
+sudo unzip smf_2-0-15_install.zip -d /var/www/html/smf
+
+sudo chown -R www-data:www-data /var/www/html/smf/
+sudo chmod -R 755 /var/www/html/smf/
+
+sudo nano /etc/apache2/sites-available/smf.conf
+
+#rédiger :
+<VirtualHost *:80>
+     ServerAdmin admin@mysmf.com
+     DocumentRoot /var/www/html/smf
+     ServerName f80.fr
+     ServerAlias forum.f80.fr
+
+     <Directory /var/www/html/smf/>
+        Options +FollowSymlinks
+        AllowOverride All
+        Require all granted
+     </Directory>
+
+     ErrorLog ${APACHE_LOG_DIR}/error.log
+     CustomLog ${APACHE_LOG_DIR}/access.log combined
+
+</VirtualHost>
+
+sudo a2ensite smf.conf
+sudo a2enmod rewrite
+
+sudo systemctl restart apache2.service
+
